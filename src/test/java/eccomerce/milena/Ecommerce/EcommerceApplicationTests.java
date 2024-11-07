95b6ed3000723c06006df87a0f1ab7943cdd7ddf
milenaa_052 <milenasantosdeoliveira40@gmail.com>
2024-11-06 23:11:18 -0300

    Iniciando o projeto

---
+++ src/test/java/eccomerce/milena/Ecommerce/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package eccomerce.milena.Ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
