df5400c6d1aa5feef41f3564d1789afe2d2af9d1
milenaa_052 <milenasantosdeoliveira40@gmail.com>
2024-11-25 23:11:46 -0300

    Corrigindo a forma de inserir e/ou atualizar a categoria dos produtos

--- src/main/java/eccomerce/milena/Ecommerce/dto/ProdutoRequestDTO.java
+++ src/main/java/eccomerce/milena/Ecommerce/dto/ProdutoRequestDTO.java
@@ -1,6 +1,4 @@
 package eccomerce.milena.Ecommerce.dto;
 
-import eccomerce.milena.Ecommerce.model.Categoria;
-
-public record ProdutoRequestDTO(String descricao, Double precoUn, Integer quantidade, String cor, Categoria categoriaId) {
+public record ProdutoRequestDTO(String descricao, Double precoUn, Integer quantidade, String cor, Integer categoriaId) {
 }
