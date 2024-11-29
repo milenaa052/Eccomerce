3b8945563fe62d5c96a4c5f7bdc37cf6180503b4
milenaa_052 <milenasantosdeoliveira40@gmail.com>
2024-11-28 22:23:32 -0300

    Separando em dois arquivos a criação da tabela e a trigger

--- src/main/resources/db/migration/V5__trigger-auditoria.sql
+++ src/main/resources/db/migration/V5__trigger-auditoria.sql
@@ -1,13 +1,3 @@
-CREATE TABLE audita_preco_produto (
-	idAuditoria INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-	dataModificacao DATE NOT NULL,
-	produtoId INT,
-	FOREIGN KEY (produtoId) REFERENCES Produtos(idProdutos),
-	precoAntigo DECIMAL(10,2),
-	PrecoNovo DECIMAL(10,2),
-	Motivo VARCHAR(255)
-)
-
 DELIMITER $$
 	CREATE OR REPLACE TRIGGER trg_audita_preco_prod
 	AFTER UPDATE ON Produtos
